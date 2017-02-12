254937db219281fb1f5129f1c15ff42c0723a6ba
Felipe Holanda Bezerra <felipe@Felipes-MacBook-Air.local>
2017-02-12 10:50:21 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>AC Fardamentos</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<!-- <link href="https://fonts.googleapis.com/css?family=Sahitya" rel="stylesheet">	 -->
+	</head>	
+	<body>
+		<header>
+			<img src="img/logo-AC-Big.png" alt="Logo da AC Fardamentos">
+			<br>
+			<br>
+			<h1>Fardamento Escolar e Camisas Promocionais</h1>
+		</header>
+		<section class="menu-bar">
+				<nav>
+					<ul>
+						<li><a href="#">Sessão 1</a></li>
+						<li><a href="#">Sessão 2</a>
+
+							 <li><a href="#">Manga curta</a></li>	
+						<li><a href="#">Manga comprida</a></li>
+						<li><a href="#">Camisa social</a></li>
+						<li><a href="#">Camisa casual</a></li>
+
+						</li>
+						<!-- <li><a href="#">Calças</a></li> -->
+						<!-- <li><a href="#">Saias</a></li> -->
+						<!-- <li><a href="#">Vestidos</a></li> -->
+						<!-- <li><a href="#">Sapatos</a></li> -->
+						<!-- <li><a href="#">Bolsas e Carteiras</a></li> -->
+						<!-- <li><a href="#">Acessórios</a></li> -->
+					</ul>
+				</nav>
+			</section>
+		<br><br><br><br><br><br><br>
+		<footer>
+			<ul>
+				<div>
+					<li>
+					E-mail:<br>acfardamentos@yahoo.com.br
+					</li>
+					<li>
+						<!-- <span class="glyphicon glyphicon-map-marker" aria-hidden="false"></span> -->
+					Endereço: Alamenda Ana Elisa, 335 - Quadra 02 - Cidade 2000 - Fortaleza - Ceará - CEP: 60.190-110
+					</li>
+				</div>
+				<div>	
+					<li>Telefone 1:<br>
+						<!-- <span class="glyphicon glyphicon-phone-alt" aria-hidden="false"></span> -->
+						3249-0668
+					</li>
+					<li> Telefone 2:<br>
+						<!-- <span class="glyphicon glyphicon-phone-alt" aria-hidden="false"></span> -->
+						3032-0654
+					</li>
+				</div>
+			</ul>
+			<h4>Desenvolvido por: Felipe Holanda Bezerra & Pedro Victor Cunha Bezerra</h4>
+		</footer>
+	</body>
+</html>
