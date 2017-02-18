8f33860f8554dc0f64b40ed44c5d86110a8b601e
Felipe Holanda Bezerra <felipe@Felipes-MacBook-Air.local>
2017-02-18 18:55:43 -0300

    correção do path na news

--- news.php
+++ news.php
@@ -24,11 +24,11 @@
 					</thead>
 					<tbody>
 						<tr>
-							<td><img src="../img/produtos/foto1.jpg" alt="Foto de Camiseta Branca"><legend>Camisetas Brancas</legend></td>
-							<td><img src="../img/produtos/foto3.jpg" alt="Foto de Camiseta Branca"><legend>Farda Colégio 7 de Setembro</legend></td>
-							<td><img src="../img/produtos/foto3.jpg" alt="Foto de Camiseta Branca"><legend>Farda Colégio Espaço Aberto</legend></td>
-							<td><img src="../img/produtos/foto4.jpg" alt="Foto de Camiseta Preta"><legend>Camisetas Pretas</legend></td>
-							<td><img src="../img/produtos/foto2.jpg" alt="Foto de Camiseta de Outras Cores"><legend>Camisetas de Outras Cores</legend></td>
+							<td><img src="img/produtos/foto1.jpg" alt="Foto de Camiseta Branca"><legend>Camisetas Brancas</legend></td>
+							<td><img src="img/produtos/foto3.jpg" alt="Foto de Camiseta Branca"><legend>Farda Colégio 7 de Setembro</legend></td>
+							<td><img src="img/produtos/foto3.jpg" alt="Foto de Camiseta Branca"><legend>Farda Colégio Espaço Aberto</legend></td>
+							<td><img src="img/produtos/foto4.jpg" alt="Foto de Camiseta Preta"><legend>Camisetas Pretas</legend></td>
+							<td><img src="img/produtos/foto2.jpg" alt="Foto de Camiseta de Outras Cores"><legend>Camisetas de Outras Cores</legend></td>
 						</tr>
 
 					</tbody>
