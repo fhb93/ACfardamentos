55eb96a1a47973a124e98a15b112da7a7d558d49
Felipe Holanda Bezerra <felipe@Felipes-MacBook-Air.local>
2017-02-26 21:09:24 -0300

    header alterado

--- header.php
+++ header.php
@@ -10,19 +10,23 @@
 	<body>
 		<div class="container-fluid">
 			<div class="row">
-				<header>
-					<div class="col-xs-6 col-sm-6 col-md-12">
-						<img src="img/logo-AC-Big.png" alt="Logo da AC Fardamentos">
-					</div>
-				
+				<div class="col-xs-12 col-md-12">
 					<div class="row">
-						<div class="col-xs-6 col-xs-offset-3 col-sm-12 col-sm-offset-0">
-							<br>
-							<h1 id="titulo-principal">Fardamento Escolar e Camisetas Promocionais</h1>
-							<br>
-						</div>
+						<header>
+							<div class="col-xs-6 col-sm-6 col-md-12">
+								<img src="img/logo-AC-Big.png" alt="Logo da AC Fardamentos">
+							</div>
+						
+							<div class="row">
+								<div class="col-xs-6 col-xs-offset-3 col-sm-12 col-sm-offset-0">
+									<br>
+									<h1 id="titulo-principal">Fardamento Escolar e Camisetas Promocionais</h1>
+									<br>
+								</div>
+							</div>
+						</header>
 					</div>
-				</header>
+				</div>
 			</div>
 		</div>
 	</body>
