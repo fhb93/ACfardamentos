8cf7d8afade952e04f98ed7198c8dbe3272c600f
Felipe Holanda Bezerra <felipe@Felipes-MacBook-Air.local>
2017-02-27 00:05:45 -0300

    layout responsivo melhorado

--- listaFardas.php
+++ listaFardas.php
@@ -8,47 +8,44 @@
 		<link rel="stylesheet" href="css/bootstrap.css">
 		<link rel="stylesheet" href="css/news.css">
 		<link rel="stylesheet" href="css/listas.css">
-		<link rel="stylesheet" type="text/css" href="css/styles.css" media="(max-device-width : 500px)">
+		<!-- <link rel="stylesheet" type="text/css" href="css/styles.css" media="(max-device-width : 500px)"> -->
 		<link rel="stylesheet" href="css/styles.css">
-		<script src="js/bootstrap.js"></script>
 		<!-- <link href="https://fonts.googleapis.com/css?family=Sahitya" rel="stylesheet">	 -->
+		<script src="js/bootstrap.js"></script>
 	</head>	
 	<body>
-		<div>
-			<?php
-				include("header.php");
-			?>
+		<?php
+			include("header.php");
+		?>
 
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-					<?php 
-						include("menu-bar.php");
-					?>
-					</div>
-					<div class="container2">
-						<div class="col-xs-11 col-xs-offset-1">
-							<div id="titulo-colegios">
-								<h1 class="h1Sobre">Lista de Fardas por Colégio</h1>
-							</div>
-							<nav>
-								<ul id="colegios">
-									<li><a href="ari.php"">Farda Escolar Ari de Sá</a></li>
-									<li><a href="farias.php">Farda Escolar Farias Brito</a></li>
-									<li><a href="kennedy.php">Farda Escolar Kennedy</a></li>
-									<li><a href="outubro.php">Farda Escolar 12 de Outubro</a></li>
-									<li><a href="shalom.php">Farda Escolar Shalom</a></li>
-								</ul>
-							</nav>
-						</div>	
-					</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+				<br><br><br><br><br>
+				<?php 
+					include("menu-bar.php");
+				?>
 				</div>
+				<!-- <div class="container2"> -->
+				<div class="col-xs-8 col-sm-8 col-sm-offset-0">
+					<!-- <div id="titulo-colegios"> -->
+						<h1 class="h1Sobre">Lista de Fardas por Colégio</h1>
+					<!-- </div> -->
+					<nav>
+						<ul class="lista-colegios">
+							<li><a href="ari.php"">Farda Escolar Ari de Sá</a></li>
+							<li><a href="farias.php">Farda Escolar Farias Brito</a></li>
+							<li><a href="kennedy.php">Farda Escolar Kennedy</a></li>
+							<li><a href="outubro.php">Farda Escolar 12 de Outubro</a></li>
+							<li><a href="shalom.php">Farda Escolar Shalom</a></li>
+						</ul>
+					</nav>
+				</div>	
 			</div>
-			<br><br>
-			<?php
-				include("footer.php");
-			?>
 		</div>
-		</script>
+		<br><br><br><br><br><br><br>
+		<?php
+			include("footer.php");
+		?>
 	</body>
 </html>
--- sobre.php
+++ sobre.php
@@ -10,34 +10,36 @@
 		<!-- <link href="https://fonts.googleapis.com/css?family=Sahitya" rel="stylesheet">	 -->
 	</head>	
 	<body>
-	<div>
 		<?php
 			include("header.php");
 		?>
-		
-		<?php 
-			include("menu-bar.php");
-		?>
-		<div>
-			<div id="textSobre">
-				<h1 class="h1Sobre">Sobre a AC Fardamentos</h1>
-				<div>
-					<div>
-					<br><br>
-						Um breve histórico:<br>
-						Há 21<!--  <?php date("Y") - 1994; ?> --> anos, uma mãe dedicada e sonhadora decidiu investir em uma antiga idéia: Oferecer qualidade a preço justo no desafiador mercado dos fardamentos. Sua antiga marca Pethigo, foi uma dos pioneiros empreendimentos no setor, na região da Cidade 2000. Após anos de esforços, a já consolidade Pethigo teve seu nome modificado para AC Fardamentos, destacando o carro-chefe da companhia, fardas escolares de qualidade e acessíveis.
-						<br>
-						Hoje, a AC Fardamentos se destaca como uma companhia em ascensão, tendo fornecido fardamentos para vários dos maiores colégios da cidade de Fortaleza. Venha conferir nossos produtos, fardas e camisetas promocionais, cuja qualidade é garantida, a variedade é extensa... E tudo isso a um custo que cabe no seu bolso!
-						<br><br><br><br><br><br><br>
-					</div>
-			 	</div>
+		<div class="container-fluid">
+			<div class="row">
+				<br><br><br><br><br>
+				<div class="col-xs-4 col-md-4">
+					<?php 
+						include("menu-bar.php");
+					?>
+				</div>
 			</div>
-		</div>
-	</div>	
-			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-	<?php
-		include("footer.php");
-	?>
+			<div class="row">
+				<div class="col-xs-8 col-md-8 col-md-offset-3">	
+					<div id="textSobre">
+						<h1 class="h1Sobre">Sobre a AC Fardamentos</h1>
+						<br><br>
+							Um breve histórico:<br>
+							Há 21<!--  <?php date("Y") - 1994; ?> --> anos, uma mãe dedicada e sonhadora decidiu investir em uma antiga idéia: Oferecer qualidade a preço justo no desafiador mercado dos fardamentos. Sua antiga marca Pethigo, foi uma dos pioneiros empreendimentos no setor, na região da Cidade 2000. Após anos de esforços, a já consolidade Pethigo teve seu nome modificado para AC Fardamentos, destacando o carro-chefe da companhia, fardas escolares de qualidade e acessíveis.
+							<br>
+							Hoje, a AC Fardamentos se destaca como uma companhia em ascensão, tendo fornecido fardamentos para vários dos maiores colégios da cidade de Fortaleza. Venha conferir nossos produtos, fardas e camisetas promocionais, cuja qualidade é garantida, a variedade é extensa... E tudo isso a um custo que cabe no seu bolso!
+					</div>
+				</div>
+			</div>
+		</div>	
+
+		<br><br><br><br><br><br><br><br>
+		<?php
+			include("footer.php");
+		?>
 	</body>
 	<script src="js/bootstrap.js"></script>
 </html>
